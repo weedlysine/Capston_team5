265a3eec3afdd53555d4fd2d6fce2492e856f948
chan821 <jc970422@gmail.com>
2022-11-30 20:57:18 +0900

    button image 추가

--- SpaceJack/Assets/Behavior Designer Movement/Scripts/Tasks/Patrol.cs
+++ SpaceJack/Assets/Behavior Designer Movement/Scripts/Tasks/Patrol.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using BehaviorDesigner.Runtime;
 
 namespace BehaviorDesigner.Runtime.Tasks.Movement
 {
@@ -14,6 +15,8 @@ namespace BehaviorDesigner.Runtime.Tasks.Movement
         public SharedFloat waypointPauseDuration = 0;
         [Tooltip("The waypoints to move to")]
         public SharedGameObjectList waypoints;
+
+        public BehaviorTree bt;
 
         // The current index that we are heading towards within the waypoints array
         private int waypointIndex;
